42dec47fd290b1db01ab7889620a8449fb9debc0
tronglinh23 <tronglinh230203@gmail.com>
2023-07-31 18:43:09 +0700

    second

---
+++ input.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+read -p "Enter your name: " NAME
+
+echo $NAME
+
+echo "Please enter your full name"
+read FNAME LNAME
+
+echo "Your full name ${FNAME} ${LNAME}"
---
+++ variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+NAME="x0n1l"
+SPORT="Foot"
+
+
+echo "My name is $NAME"
+# conjunction
+echo "The most popular sport ${SPORT}ball"
